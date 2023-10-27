aac98a9c7847b462efa1c61a4263bd6fbea80c67
伍侃 <wu-kan@163.com>
2023-10-27 14:05:06 +0800

    Update joinquant_playwright.py

--- examples/joinquant_playwright.py
+++ examples/joinquant_playwright.py
@@ -1,11 +1,18 @@
-"""
+r"""
 # 安装
 pip install playwright
 
-# 登录
+# 使用
 1. 修改代码中 chrome 的路径
 2. 修改代码中的手机号与密码
 3. 修改`xxx.ipynb`为你的Notebook中名字
+
+# 批处理示例
+call d:\Users\Kan\miniconda3\Scripts\activate.bat D:\Users\Kan\miniconda3\envs\py38
+python D:\Users\Kan\Documents\GitHub\examples\joinquant_playwright.py
+python -c "import time;print('+++++'),time.sleep(5),print('=====')"
+pause
+
 """
 
 import subprocess
@@ -13,38 +20,52 @@ import time
 
 from playwright.sync_api import Playwright, sync_playwright
 
-# 这个路径可以是Google浏览器的exe路径，也可以是快捷方式的路径
-chrome_path = r'"C:\Program Files\Google\Chrome\Application\chrome.exe"'
-debugging_port = "--remote-debugging-port=9222"
-command = f"{chrome_path} {debugging_port}"
-subprocess.Popen(command, shell=True)
-time.sleep(2)
+# 远程桌面
+IS_RDP = False
+USERNAME = "13912345678"
+PASSWORD = "123456"
+NOTEBOOK = "ksrpc_run.ipynb"
+CHROME_PATH = r'C:\Program Files\Google\Chrome\Application\chrome.exe'
 
 
 def run(playwright: Playwright) -> None:
-    # browser = playwright.chromium.launch(channel="msedge", headless=False, slow_mo=1)
-    # context = browser.new_context()
-    # page = context.new_page()
+    if IS_RDP:
+        browser = playwright.chromium.launch(slow_mo=1, headless=True, executable_path=CHROME_PATH)
+        context = browser.new_context()
+        page = context.new_page()
+    else:
+        command = f'"{CHROME_PATH}" --remote-debugging-port=9222'
+        subprocess.Popen(command, shell=True)
+        time.sleep(2)
 
-    # 一次只能接一个，所以已经打开的Chrome浏览器最好先关闭
-    browser = playwright.chromium.connect_over_cdp("http://localhost:9222", slow_mo=1000)
-    context = browser.contexts[0]
-    page = context.pages[0]
+        # 一次只能接一个，所以已经打开的Chrome浏览器最好先关闭
+        browser = playwright.chromium.connect_over_cdp("http://localhost:9222", slow_mo=1000)
+        context = browser.contexts[0]
+        page = context.pages[0]
 
     page.goto("https://www.joinquant.com/research")
     page.get_by_role("textbox", name="手机号").click()
-    page.get_by_role("textbox", name="手机号").fill("13912345678")
+    page.get_by_role("textbox", name="手机号").fill(USERNAME)
     page.get_by_placeholder("请输入密码").click()
-    page.get_by_placeholder("请输入密码").fill("123456")
+    page.get_by_placeholder("请输入密码").fill(PASSWORD)
     page.get_by_role("button", name="登 录").click()
     with page.expect_popup() as page1_info:
-        page.frame_locator("iframe[name=\"research\"]").get_by_role("link", name="ksrpc_run.ipynb").click()
+        page.frame_locator("iframe[name=\"research\"]").get_by_role("link", name=NOTEBOOK).click()
     page1 = page1_info.value
     print('可能网页没有加载完，等待一会')
     page1.wait_for_load_state("load")
-    page1.frame_locator("iframe[name=\"research\"]").get_by_role("link", name="内核").click()
-    page1.frame_locator("iframe[name=\"research\"]").get_by_role("link", name="重启并运行所有").click()
-    page1.frame_locator("iframe[name=\"research\"]").get_by_role("button", name="重启并运行所有单元格").click()
+    if IS_RDP:
+        print('通过JS注入 重启内核')
+        page1.wait_for_timeout(3000)
+        page1.evaluate('() => this.frames[0].Jupyter.notebook.restart_run_all({confirm: false})')
+        page1.wait_for_timeout(3000)
+    else:
+        print('通过界面点击 重启内核')
+        page1.frame_locator("iframe[name=\"research\"]").get_by_role("link", name="内核").click()
+        page1.frame_locator("iframe[name=\"research\"]").get_by_role("link", name="重启并运行所有").click()
+        page1.wait_for_timeout(3000)
+        print('headless模式下，此按钮居然找不到')
+        page1.frame_locator("iframe[name=\"research\"]").get_by_role("button", name="重启并运行所有单元格").click()
 
     # ---------------------
     print('已完成')

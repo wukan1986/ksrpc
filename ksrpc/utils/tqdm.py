222fad6b04ebdd91581e4cb5f99576f7dfa3b940
Kan <wu-kan@163.com>
2025-09-21 17:12:49 +0800

    Update tqdm.py

--- ksrpc/utils/tqdm.py
+++ ksrpc/utils/tqdm.py
@@ -9,7 +9,7 @@ def update_progress(i, print, file=sys.stderr):
     """简化版进度条"""
     j = i % 10
     if j == 0:
-        print('=', end='', file=file)
+        print('0', end='', file=file)
     elif j == 9:
         print('\b=', end='', file=file)
     else:

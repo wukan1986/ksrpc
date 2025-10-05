58f9aa73c37fa8e5d0a5787d3b6c6fca3cf011c4
Kan <wu-kan@163.com>
2025-10-06 01:45:15 +0800

    优化服务端进度条显示

--- ksrpc/utils/chunks.py
+++ ksrpc/utils/chunks.py
@@ -3,12 +3,13 @@ import sys
 import zlib
 from io import BytesIO
 
+from ksrpc.utils.misc import format_number
 from ksrpc.utils.tqdm import update_progress
 
 
 async def data_sender(data, print, chunk_size=1024 * 128):
     file = sys.stderr
-    print(f'加载数据: ({len(data):,} bytes) [', end='', file=file)
+    print(f'加载数据: ({format_number(len(data))}B) [', end='', file=file)
     data = BytesIO(data)
     chunk = data.read(chunk_size)
     i = -1
@@ -21,14 +22,14 @@ async def data_sender(data, print, chunk_size=1024 * 128):
         update_progress(i, print, file=file)
         chunk = data.read(chunk_size)
     # 会快速的显示到此处，但实际数据还在发送中，建议与客户端接收进度一起看
-    print(f'] 压缩完成 ({size:,} bytes) | 底层异步发送中，请勿立即退出！', file=file)
+    print(f'] 压缩完成 ({format_number(size)}B) | 底层异步发送中，请勿立即退出！', file=file)
 
 
 async def send_in_chunks(ws, data, print, chunk_size=1024 * 128):  # 32KB
     # 64KB时，某宽发出的大数据无法zlib解压，所以改成了32KB
     # 将数据分块发送
     file = sys.stderr
-    print(f'发送数据: ({len(data):,} bytes) [', end='', file=file)
+    print(f'发送数据: ({format_number(len(data))}B) [', end='', file=file)
 
     j = -1
     size = 0
@@ -41,7 +42,7 @@ async def send_in_chunks(ws, data, print, chunk_size=1024 * 128):  # 32KB
         j += 1
         update_progress(j, print, file=file)
     # 会快速的显示到此处，但实际数据还在发送中，建议与客户端接收进度一起看
-    print(f'] 压缩完成 ({size:,} bytes) | 底层异步发送中，请勿立即退出！', file=file)
+    print(f'] 压缩完成 ({format_number(size)}B) | 底层异步发送中，请勿立即退出！', file=file)
 
     # 发送结束标志
     await ws.send_str("EOF")

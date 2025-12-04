3c9d1c0813047bdb313dd9bbb86f8679044fdabc
Kan <wu-kan@163.com>
2025-12-04 16:18:04 +0800

    Update demo_jqresearch.py

--- examples/demo_jqresearch.py
+++ examples/demo_jqresearch.py
@@ -1,11 +1,15 @@
 import nest_asyncio
 
-from examples.config import USERNAME, PASSWORD, URL_HTTP, URL_WS  # noqa
 from ksrpc.client import RpcClient
 from ksrpc.connections.http import HttpConnection  # noqa
 from ksrpc.connections.websocket import WebSocketConnection  # noqa
 
 URL_HTTP = 'http://100.100.100.100:7001/api/v1/{time}'
+URL_HTTP = 'http://frp.freefrp.net:7001/api/v1/{time}'
+URL_HTTP = 'http://us.afrp.net:7001/api/v1/{time}'
+
+USERNAME = 'admin'
+PASSWORD = 'password123'
 
 # 必用，否则同步模式只能调用第一次，第二次会报 RuntimeError: Event loop is closed
 nest_asyncio.apply()
